90a5de2748b36bfc43749f71b5885b6761659354
amirdi <amir.dirin@yahoo.com>
2025-03-24 20:49:10 +0200

    Bug fixed

--- src/main/java/com/example/localdemoenkoin/HelloApplication.java
+++ src/main/java/com/example/localdemoenkoin/HelloApplication.java
@@ -12,7 +12,7 @@ import java.util.ResourceBundle;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-       //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+       FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("hello-view.fxml"));
--- src/main/java/com/example/localdemoenkoin/HelloController.java
+++ src/main/java/com/example/localdemoenkoin/HelloController.java
@@ -3,66 +3,59 @@ package com.example.localdemoenkoin;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Border;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
-
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class HelloController {
+    @FXML private Label welcomeText;
+    @FXML private Button button1;
+    @FXML private Button button2;
+    @FXML private Button button3;
+
+    private ResourceBundle resourceBundle;
+
     @FXML
-    private Label welcomeText;
+    public void initialize() {
+        System.out.println("Initializing Controller...");
+        System.out.println("button1: " + button1); // Debugging line
+        System.out.println("button2: " + button2);
+        System.out.println("button3: " + button3);
 
-    public Button button1;
-    public Button button2;
-    public Button button3;
+        if (button1 == null || button2 == null || button3 == null) {
+            System.err.println("FXML components are NOT properly injected!");
+        } else {
+            loadLanguage("en", "US"); // Default language
+        }
+    }
 
+    private void loadLanguage(String langCode, String country) {
+        Locale locale = new Locale(langCode, country);
+        resourceBundle = ResourceBundle.getBundle("com.example.localdemoenkoin.bundle1", locale);
 
-    public void initialize(){
-//        ResourceBundle r = ResourceBundle.getBundle("bundle1");
-//        button1.setText(r.getString("button1.text"));
-//        button2.setText(r.getString("button2.text"));
-//        button3.setText(r.getString("button3.text"));
-
+        if (button1 != null && button2 != null && button3 != null) {
+            button1.setText(resourceBundle.getString("button1.text"));
+            button2.setText(resourceBundle.getString("button2.text"));
+            button3.setText(resourceBundle.getString("button3.text"));
+            welcomeText.setText(resourceBundle.getString("label.text"));
+        } else {
+            System.err.println("Buttons are null! Check FXML injection.");
+        }
     }
 
     @FXML
-
-    protected void onHelloButtonClick() {
-        welcomeText.setTextFill(Color.rgb(255,0,0));
-        welcomeText.setFont(Font.font(14));
-        welcomeText.setText("Use of ResourceBundle");
-
-//        ResourceBundle r = ResourceBundle.getBundle("bundle1");
-//        button1.setText(r.getString("button1.text"));
-//        button2.setText(r.getString("button2.text"));
-//        button3.setText(r.getString("button3.text"));
+    protected void btnHiEn() {
+        loadLanguage("en", "US"); // English
     }
+
     @FXML
-    protected void btnHi() {
-        welcomeText.setTextFill(Color.rgb(0,255,0));
-        welcomeText.setFont(Font.font(16));
-        welcomeText.setText("Hi,  Welcome to JavaFX ResourceBundle!");
+    protected void btnMoiFi() {
+        loadLanguage("sv", "SE"); // Swedish
     }
+
     @FXML
-    protected void btnTerv() {
-        welcomeText.setTextFill(Color.rgb(0,0,255));
-        welcomeText.setFont(Font.font(18));
-
-        welcomeText.setText("Hej, Välkommen till JavaFX ResourceBundle!!");
-
+    protected void btnHeiSE() {
+        loadLanguage("fi", "FI"); // Finnish
     }
-    @FXML
-    protected void btnVel() {
-        welcomeText.setTextFill(Color.rgb(200,120,120));
-        welcomeText.setFont(Font.font(14));
-
-        welcomeText.setText("Moi, Tervetuloa JavaFX ResourceBundleen!!");
-
-
-    }
-
-
 }
-
